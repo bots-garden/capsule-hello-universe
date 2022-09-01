40e3d0e1dba506fb1a5643bc1d37a7ea0a4e693a
Philippe Charrière <ph.charriere@gmail.com>
2022-09-01 16:31:49 +0000

    📝 save

--- serve-bundle/resources/index.js
+++ serve-bundle/resources/index.js
@@ -1,1 +1,58 @@
+import { LitElement, html, css} from 'lit-element';
+import { myStyle } from './style.js';
+
 console.log("👋 Hello World 🌍")
+
+class HelloWorld extends LitElement {
+  static styles = [myStyle]
+
+  render() {
+    return html`
+    <h1 class="title">👋 Hello World 🌍</h1>
+    `;
+  }
+}
+
+customElements.define('hello-world', HelloWorld);
+
+class ServedByCapsule extends LitElement {
+  static styles = [myStyle]
+
+  render() {
+    return html`
+    <h2 class="subtitle">Served with 💜 by Capsule 💊</h2>
+    `;
+  }
+}
+
+customElements.define('served-by-capsule', ServedByCapsule);
+
+class LittleMessage extends LitElement {
+  static styles = [myStyle]
+
+  render() {
+    return html`
+    <h2 class="subtitle"> {{message}} </h2>
+    `;
+  }
+}
+
+customElements.define('little-message', LittleMessage);
+
+class MainApp extends LitElement {
+  static styles = [myStyle]
+
+  render() {
+    return html`
+    <section class="container">
+      <div>
+        <hello-world></hello-world>
+        <served-by-capsule></served-by-capsule>
+        <little-message></little-message>
+      </div>
+    </section>    
+    `;
+  }
+}
+
+customElements.define('main-app', MainApp);
